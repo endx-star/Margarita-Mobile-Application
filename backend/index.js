d7a52da10dcf774e97a8a84d59a0ce56c5768aff
Endale Teggnework <endalework90@gmail.com>
2023-05-03 15:24:28 +0300

    Configuration enviroment added

--- backend/index.js
+++ backend/index.js
@@ -3,19 +3,23 @@ const app = express()
 const port = process.env.PORT || 8080
 const cors = require('cors')
 const bodyParser = require('body-parser')
-
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
-mongoose.connect(
-  // 'mongodb://localhost:27017/margaritaApp',
-  // 'mongodb+srv://abrarbilalmicheal:abrarbilalmicheal@cluster0.16zft.mongodb.net/petaffixdb?retryWrites=true&w=majority',
-  'mongodb+srv://margaritamayhemcompany:Iqpq58k8WfbW66Zm@cluster0.3jntivp.mongodb.net/petaffixdb?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  },
-),
+
+dotenv.config({ path: './config.env' })
+const app = require('./app')
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+)
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}),
   app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
